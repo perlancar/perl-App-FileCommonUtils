20bd1f0d8445e2dc273bd35b9d833436b806ebdb
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-16 13:59:40 +0700

    Initial commit

---
+++ lib/App/FileCommonUtils.pm
@@ -0,0 +1,16 @@
+package App::FileCommonUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs for File::Common
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities:
+
+#INSERT_EXECS_LIST
